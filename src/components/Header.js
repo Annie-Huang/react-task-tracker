03069749d63ed8e4bb1a8a5113bce4ce60d731c0
Annie Huang <annie.hn@gmail.com>
2021-01-20 22:31:58 +1100

    Just keep it as not for inline style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
 
 const Header = ({ title }) => {
-  // <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
   return (
     <header>
-      <h1 style={headingStyle}>{title}</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
@@ -16,5 +15,9 @@ Header.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
-const headingStyle = { color: 'red', backgroundColor: 'black' };
+// CSS in JS. You generally only use inline style for dynamic variables
+// <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
+/*<h1 style={headingStyle}>{title}</h1>
+const headingStyle = { color: 'red', backgroundColor: 'black' };*/
+
 export default Header;
