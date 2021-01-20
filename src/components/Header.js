36312262992222f2e7dd1ebcb25beba4a56cce41
Annie Huang <annie.hn@gmail.com>
2021-01-20 22:29:25 +1100

    Abstract the inline style into a constant

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
+  // <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
   return (
     <header>
-      <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
+      <h1 style={headingStyle}>{title}</h1>
     </header>
   );
 };
 
 Header.defaultProps = {
-  title: 'Task Tracker'
-}
+  title: 'Task Tracker',
+};
 Header.propTypes = {
-  title: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+};
+
+const headingStyle = { color: 'red', backgroundColor: 'black' };
 export default Header;
