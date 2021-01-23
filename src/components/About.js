6755ad582bc70926f2137141638b4b249ffd0c50
Annie Huang <annie.hn@gmail.com>
2021-01-23 23:00:30 +1100

    Use <Link> rather <a> to stop full page reload.

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
+// Use <Link> rather <a> to stop full page reload.
 const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
-      <a href='/'>Go Back</a>
+      {/*<a href='/'>Go Back</a>*/}
+      <Link to='/'>Go Back</Link>
     </div>
   );
 };
